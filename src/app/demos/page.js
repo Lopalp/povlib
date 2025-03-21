edec1ae9bd2d96749f824ea3e5b422ca9ab32ea3
7lopalp7 <7lopalp7@gmail.com>
2025-03-21 13:38:00 +0000

    added Demos page

---
+++ src/app/demos/page.js
@@ -0,0 +1,10 @@
+import DemosIndex from '@/components/POVlib/DemosIndex';
+
+export const metadata = {
+  title: 'CS2 POV Demos | POVlib',
+  description: 'Browse and filter all CS2 professional and community point-of-view (POV) demos. Find top plays, map strategies, and player highlights.',
+}
+
+export default function DemosPage() {
+  return <DemosIndex />;
+}
