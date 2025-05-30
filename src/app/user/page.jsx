1022cd7d6d6d27d33c26dfb51fe3d02db3b8c547
7lopalp7 <7lopalp7@gmail.com>
2025-05-30 09:28:32 +0000

    update

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,7 +1,7 @@
 jsx
 import React from 'react';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import Navbar from '../../components/POVlib/Navbar';
+import Footer from '../../components/POVlib/Footer';
 
 const UserPage = ({ user }) => {
   return (
