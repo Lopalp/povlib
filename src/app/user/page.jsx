a547722138691a4602480e484e83d560eee02a54
7lopalp7 <7lopalp7@gmail.com>
2025-05-30 09:58:48 +0000

    update

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -103,7 +103,7 @@ const UserPage = () => {
             <div className="mt-12">
               <h2 className="text-2xl font-bold mb-6">Deine POVs</h2>
               {demos.length > 0 ? (
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grpid-cols-3 xl:grid-cols-4 gap-6">
                   {demos.map((demo) => (
                     <DemoCard
                       key={demo.id}
