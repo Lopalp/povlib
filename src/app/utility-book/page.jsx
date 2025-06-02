0aa58e2dd5580f871525bdedb2d0b17f9f5f3cfd
7lopalp7 <7lopalp7@gmail.com>
2025-06-02 11:01:29 +0000

    update

---
+++ src/app/utility-book/page.jsx
@@ -0,0 +1,9 @@
+// src/app/utility-book/page.jsx
+'use client';
+
+import React from 'react';
+import UtilityBookPage from '@/components/POVlib/UtilityBookPage';
+
+export default function UtilityBookRoute() {
+  return <UtilityBookPage />;
+}
