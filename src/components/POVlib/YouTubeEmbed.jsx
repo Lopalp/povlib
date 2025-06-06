3d799f8a9d425d59c0b0c2ac322dc8aad853f29d
7lopalp7 <7lopalp7@gmail.com>
2025-06-06 12:00:41 +0000

    update

--- src/components/POVlib/YouTubeEmbed.jsx
+++ src/components/POVlib/YouTubeEmbed.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
-const YouTubeEmbed = ({ videoId, title, autoplay = false, className = "", controls = true, showInfo = false }) => {
-
+const YouTubeEmbed = ({
+  videoId,
+  title,
+  autoplay = false,
+  className = "",
+  controls = true,
+  showInfo = false,
+}) => {
   return (
-    <div className={`relative w-full pb-[56.25%] h-0 rounded-lg overflow-hidden ${className}`}>
+    <div className={`relative w-full h-full sm:pb-[56.25%] sm:h-0 overflow-hidden ${className}`}>
       <iframe
-        className="absolute top-0 left-0 w-full h-full rounded-lg"
+        className="absolute top-0 left-0 w-full h-full"
         src={`https://www.youtube.com/embed/${videoId}?autoplay=${autoplay ? 1 : 0}&rel=0&controls=${controls ? 1 : 0}&showinfo=${showInfo ? 1 : 0}&mute=${autoplay ? 1 : 0}&modestbranding=1`}
         title={title || "YouTube video player"}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
         allowFullScreen
-      ></iframe>
+      />
     </div>
   );
 };
