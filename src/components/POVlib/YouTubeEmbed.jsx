25702c5ba76cb93e8aba81ad8a643706796369e0
7lopalp7 <7lopalp7@gmail.com>
2025-06-06 12:20:33 +0000

    update

--- src/components/POVlib/YouTubeEmbed.jsx
+++ src/components/POVlib/YouTubeEmbed.jsx
@@ -10,20 +10,15 @@ const YouTubeEmbed = ({
 }) => {
   return (
     <div className={`relative w-full h-full overflow-hidden ${className}`}>
-      <iframe
-        className="
-          absolute
-          top-0
-          left-1/2
-          h-full
-          w-auto
-          -translate-x-1/2
-        "
-        src={`https://www.youtube.com/embed/${videoId}?autoplay=${autoplay ? 1 : 0}&rel=0&controls=${controls ? 1 : 0}&showinfo=${showInfo ? 1 : 0}&mute=${autoplay ? 1 : 0}&modestbranding=1`}
-        title={title || "YouTube video player"}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-        allowFullScreen
-      />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 scale-[1.35] sm:scale-100 w-full h-full">
+        <iframe
+          className="w-full h-full"
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=${autoplay ? 1 : 0}&rel=0&controls=${controls ? 1 : 0}&showinfo=${showInfo ? 1 : 0}&mute=${autoplay ? 1 : 0}&modestbranding=1`}
+          title={title || "YouTube video player"}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+          allowFullScreen
+        />
+      </div>
     </div>
   );
 };
