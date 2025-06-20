dc42709ffa587a52dccb337c75c6ec486ca8aa1d
7lopalp7 <7lopalp7@gmail.com>
2025-06-20 08:01:29 +0000

    analytics

--- src/components/layout/GlobalLayout.jsx
+++ src/components/layout/GlobalLayout.jsx
@@ -6,6 +6,8 @@ import Navbar from "../navigation/Navbar";
 import Footer from "../navigation/Footer";
 import Sidebar from "../navigation/Sidebar";
 import { NavbarProvider, useNavbar } from "../../context/NavbarContext";
+import { Analytics } from "@vercel/analytics/next"
+
 
 function GlobalLayoutContent({ children }) {
   const pathname = usePathname();
@@ -62,6 +64,7 @@ function GlobalLayoutContent({ children }) {
 export default function GlobalLayout({ children }) {
   return (
     <NavbarProvider>
+      <Analytics/>
       <GlobalLayoutContent>{children}</GlobalLayoutContent>
     </NavbarProvider>
   );
